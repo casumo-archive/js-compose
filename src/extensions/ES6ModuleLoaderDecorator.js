c75a3ff160ee0823cd1a3fa2905296510c53caef
Daniel Howlett <dan@dancras.co.uk>
2017-06-19 20:58:05 +0200

    Add ES6ModuleLoaderDecorator to support modules with default export

---
+++ src/extensions/ES6ModuleLoaderDecorator.js
@@ -0,0 +1,31 @@
+/**
+ * Use this to support ES6 default exports in other module loaders.
+ *
+ * Note. This implementation could have unexpected results if userland code
+ * uses the key "default".
+ */
+export default class ES6ModuleLoaderDecorator {
+
+    constructor (decorated) {
+        this.decorated = decorated;
+    }
+
+    canLoadModule (extensionApi) {
+        return this.decorated.canLoadModule(extensionApi);
+    }
+
+    loadModule (extensionApi) {
+
+        return this.decorated.loadModule(extensionApi).then((loadedModule) => {
+
+            if (loadedModule.default) {
+                return loadedModule.default;
+            }
+
+            return loadedModule;
+
+        });
+
+    }
+
+}
---
+++ src/extensions/es6ModuleLoaderDecoratorSpec.js
@@ -0,0 +1,62 @@
+/* eslint no-unused-expressions: 0, max-nested-callbacks: 0 */
+/* eslint-env mocha */
+
+import { containerDoubles } from '../../test/doubles';
+import ES6ModuleLoaderDecorator from './ES6ModuleLoaderDecorator';
+
+describe('ES6ModuleLoaderDecorator', () => {
+
+    let decorated;
+    let loader;
+    let extensionApi;
+
+    beforeEach(() => {
+        decorated = containerDoubles.moduleLoader();
+        loader = new ES6ModuleLoaderDecorator(decorated);
+    });
+
+    describe('canLoadModule', () => {
+
+        it('should return true when decorated extension canLoadModule returns true', () => {
+            const extensionApi = containerDoubles.extensionApi();
+
+            decorated.canLoadModule.withArgs(extensionApi).returns(true);
+            loader.canLoadModule(extensionApi).should.equal(true);
+        });
+
+        it('should return false when decorated extension canLoadModule returns false', () => {
+            decorated.canLoadModule.withArgs(extensionApi).returns(false);
+            loader.canLoadModule(extensionApi).should.equal(false);
+        });
+
+    });
+
+    describe('loadModule', () => {
+
+        it('should return the default export if default available', () => {
+
+            const extensionApi = containerDoubles.extensionApi();
+            const expected = {};
+
+            decorated.loadModule.withArgs(extensionApi).resolves({
+                default: expected
+            });
+
+            return loader.loadModule(extensionApi).should.eventually.equal(expected);
+
+        });
+
+        it('should return the module from decorated if default not available', () => {
+
+            const extensionApi = containerDoubles.extensionApi();
+            const expected = {};
+
+            decorated.loadModule.withArgs(extensionApi).resolves(expected);
+
+            return loader.loadModule(extensionApi).should.eventually.equal(expected);
+
+        });
+
+    });
+
+});
