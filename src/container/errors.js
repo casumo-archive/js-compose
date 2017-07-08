952ba1bd5968f1b9e55be092fa6985b77f80fc05
Daniel Howlett <dan@dancras.co.uk>
2017-07-08 11:23:39 +0200

    Add ServiceError class

--- src/container/errors.js
+++ src/container/errors.js
@@ -1,13 +1,26 @@
+export class ServiceError extends Error {
+    constructor (serviceId, e) {
+
+        super(`Service '${serviceId}', ${e.message}`);
+
+        if (e instanceof ServiceError) {
+            return e;
+        }
+
+        this.stack = e.stack;
+
+    }
+}
+
 export class ArgError extends Error {
     constructor (argDefinition, e) {
 
         super(`Arg '${argDefinition}', ${e.message}`);
 
-        if (e instanceof ArgError) {
+        if (e instanceof ArgError || e instanceof ServiceError) {
             return e;
         }
 
-        this.innerError = new Error(this.message);
         this.stack = e.stack;
 
     }
--- src/container/errorsSpec.js
+++ src/container/errorsSpec.js
@@ -1,7 +1,45 @@
 /* eslint no-unused-expressions: 0 */
 /* eslint-env mocha */
 
-import { ArgError } from './errors';
+import { ArgError, ServiceError } from './errors';
+
+describe('ServiceError', () => {
+
+    it('should include the service id in its message', () => {
+
+        const e = new ServiceError('foo', new Error('bar'));
+
+        e.message.should.contain('foo');
+
+    });
+
+    it('should include the original error message in its message', () => {
+
+        const e = new ServiceError('foo', new Error('bar'));
+
+        e.message.should.contain('bar');
+
+    });
+
+    it('should use the original error for its stack', () => {
+
+        const original = new Error('bar');
+        const e = new ServiceError('foo', original);
+
+        e.stack.should.equal(original.stack);
+
+    });
+
+    it('should use the original entirely if its a ServiceError', () => {
+
+        const original = new ServiceError('foo', new Error('bar'));
+        const e = new ServiceError('123', original);
+
+        e.should.equal(original);
+
+    });
+
+});
 
 describe('ArgError', () => {
 
@@ -35,8 +73,16 @@ describe('ArgError', () => {
         const original = new ArgError('foo', new Error('bar'));
         const e = new ArgError('123', original);
 
-        e.message.should.equal(original.message);
-        e.stack.should.equal(original.stack);
+        e.should.equal(original);
+
+    });
+
+    it('should use the original entirely if its a ServiceError', () => {
+
+        const original = new ServiceError('foo', new Error('bar'));
+        const e = new ArgError('123', original);
+
+        e.should.equal(original);
 
     });
 
