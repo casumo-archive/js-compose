79d10cfd760b1cc4c2479afe4dd5aeb8cd4c7f5b
Levente Balogh <balogh.levente.hu@gmail.com>
2017-08-11 08:28:27 +0200

    Container: use composition instead of an ES6 class
    The reason for this is that ES6 class methods
    are not enumerable, and as we are creating new
    instances of the container in the code by using _.extend(),
    the inherited & not enumerable properties are not copied.
    This way it is more straightforward.

--- src/container/Container.js
+++ src/container/Container.js
@@ -6,204 +6,204 @@ function countOccurrences (array, item) {
     return array.filter(_.isEqual.bind(_, item)).length;
 }
 
-export default class Container {
-
-    constructor (extensions, config) {
-        this.moduleLoaders = _.filter(extensions, _.property('canLoadModule'));
-        this.argResolvers = _.filter(extensions, _.property('canResolveArg'));
-        this.initialisers = _.filter(extensions, _.property('canInitialise'));
-        this.extraHandlers = _.filter(extensions, _.property('canHandleExtra'));
-        this.config = config;
-        this.cache = {};
-        this.chain = [];
+export default function Container (extensions, config) {
+    return {
+        moduleLoaders: _.filter(extensions, 'canLoadModule'),
+        argResolvers: _.filter(extensions, _.property('canResolveArg')),
+        initialisers: _.filter(extensions, _.property('canInitialise')),
+        extraHandlers: _.filter(extensions, _.property('canHandleExtra')),
+        config: config,
+        cache: {},
+        chain: [],
+        get,
+        lint
     }
+}
 
-    /**
-     * @param {String} id
-     *
-     * @return {Promise}
-     */
-    get (id) {
+/**
+ * @param {String} id
+ *
+ * @return {Promise}
+ */
+function get (id) {
 
-        const self = this;
-        const definition = self.config.services[id];
-        let mappedExtraHandlers;
-        const extensionApi = new ExtensionApi(self, id, definition);
+    const self = this;
+    const definition = self.config.services[id];
+    let mappedExtraHandlers;
+    const extensionApi = new ExtensionApi(self, id, definition);
 
-        const output = self.cache[id] || new Promise((resolve) => {
+    const output = self.cache[id] || new Promise((resolve) => {
 
-            if (!definition) {
-                throw new Error('Missing definition');
-            }
+        if (!definition) {
+            throw new Error('Missing definition');
+        }
 
-            if (countOccurrences(self.chain, id) > 1) {
-                throw new Error(`Circular dependency detected: ${self.chain.concat(id).join(', ')}`);
-            }
+        if (countOccurrences(self.chain, id) > 1) {
+            throw new Error(`Circular dependency detected: ${self.chain.concat(id).join(', ')}`);
+        }
 
-            mappedExtraHandlers = _.map(definition.extras || [], (extraDefinition) => {
+        mappedExtraHandlers = _.map(definition.extras || [], (extraDefinition) => {
 
-                const handler = _.find(self.extraHandlers, (extraHandler) => {
-                    return extraHandler.canHandleExtra(extraDefinition, extensionApi);
-                });
-
-                if (!handler) {
-                    throw new Error(`No extra handler for ${extraDefinition}`);
-                }
-
-                return handler;
-
+            const handler = _.find(self.extraHandlers, (extraHandler) => {
+                return extraHandler.canHandleExtra(extraDefinition, extensionApi);
             });
 
-            const moduleLoader = _.find(self.moduleLoaders, (moduleLoader) => {
-                return moduleLoader.canLoadModule(extensionApi);
-            });
-
-            if (!moduleLoader) {
-                throw new Error('No module loader');
+            if (!handler) {
+                throw new Error(`No extra handler for ${extraDefinition}`);
             }
 
-            const promises = extensionApi.resolveArgs(definition.args || []);
+            return handler;
 
-            promises.unshift(moduleLoader.loadModule(extensionApi));
-
-            const initialiser = _.find(self.initialisers, (initialiser) => {
-                return initialiser.canInitialise(extensionApi);
-            });
-
-            if (!initialiser) {
-                throw new Error('No initialiser');
-            }
-
-            self.cache[id] = Promise.all(promises).then((contents) => {
-
-                const initialisedPromises = _.map(mappedExtraHandlers, (handler, extraIndex) => {
-                    if (handler.beforeServiceInitialised) {
-                        return handler.beforeServiceInitialised(definition.extras[extraIndex], extensionApi);
-                    }
-                });
-
-                return Promise.all(initialisedPromises).then(() => {
-                    return contents;
-                });
-
-            }).then((contents) => {
-
-                return initialiser.initialise(
-                    // eslint-disable-next-line prefer-arrow-callback
-                    function instanceCreatedCallback (instance) {
-
-                        mappedExtraHandlers.forEach((handler, extraIndex) => {
-
-                            if (handler.onServiceInstanceCreated) {
-
-                                handler.onServiceInstanceCreated(
-                                    instance,
-                                    definition.extras[extraIndex],
-                                    extensionApi
-                                );
-
-                            }
-
-                        });
-
-                    },
-                    ...contents
-                );
-
-            }).then((instance) => {
-
-                const initialisedPromises = _.map(mappedExtraHandlers, (handler, extraIndex) => {
-                    if (handler.onServiceInitialised) {
-                        return handler.onServiceInitialised(
-                            instance,
-                            definition.extras[extraIndex],
-                            extensionApi
-                        );
-                    }
-                });
-
-                return Promise.all(initialisedPromises).then(() => {
-                    return instance;
-                });
-
-            });
-
-            resolve(self.cache[id]);
-
-        }).then(null, (error) => {
-            throw new ServiceError(id, error);
         });
 
-        _.each(mappedExtraHandlers, (handler, extraIndex) => {
-            if (handler.onGetComplete) {
-                handler.onGetComplete(definition.extras[extraIndex], extensionApi);
-            }
+        const moduleLoader = _.find(self.moduleLoaders, (moduleLoader) => {
+            return moduleLoader.canLoadModule(extensionApi);
         });
 
-        return output;
-    }
+        if (!moduleLoader) {
+            throw new Error('No module loader');
+        }
 
-    /**
-     * @return {Promise<Array<String>>} - A list of lint errors
-     */
-    lint () {
+        const promises = extensionApi.resolveArgs(definition.args || []);
 
-        return Promise.all(_.map(this.config.services, (serviceDefinition, serviceId) => {
+        promises.unshift(moduleLoader.loadModule(extensionApi));
 
-            const errors = [];
-            const extensionApi = new ExtensionApi(this, serviceId, serviceDefinition);
+        const initialiser = _.find(self.initialisers, (initialiser) => {
+            return initialiser.canInitialise(extensionApi);
+        });
 
-            const moduleLoader = _.find(this.moduleLoaders, (moduleLoader) => {
-                return moduleLoader.canLoadModule(extensionApi);
+        if (!initialiser) {
+            throw new Error('No initialiser');
+        }
+
+        self.cache[id] = Promise.all(promises).then((contents) => {
+
+            const initialisedPromises = _.map(mappedExtraHandlers, (handler, extraIndex) => {
+                if (handler.beforeServiceInitialised) {
+                    return handler.beforeServiceInitialised(definition.extras[extraIndex], extensionApi);
+                }
             });
 
-            if (!moduleLoader) {
-                errors.push(`Missing module loader for ${serviceId}`);
-            } else if (moduleLoader.lintLoader) {
-                errors.push(moduleLoader.lintLoader(extensionApi));
+            return Promise.all(initialisedPromises).then(() => {
+                return contents;
+            });
+
+        }).then((contents) => {
+
+            return initialiser.initialise(
+                // eslint-disable-next-line prefer-arrow-callback
+                function instanceCreatedCallback (instance) {
+
+                    mappedExtraHandlers.forEach((handler, extraIndex) => {
+
+                        if (handler.onServiceInstanceCreated) {
+
+                            handler.onServiceInstanceCreated(
+                                instance,
+                                definition.extras[extraIndex],
+                                extensionApi
+                            );
+
+                        }
+
+                    });
+
+                },
+                ...contents
+            );
+
+        }).then((instance) => {
+
+            const initialisedPromises = _.map(mappedExtraHandlers, (handler, extraIndex) => {
+                if (handler.onServiceInitialised) {
+                    return handler.onServiceInitialised(
+                        instance,
+                        definition.extras[extraIndex],
+                        extensionApi
+                    );
+                }
+            });
+
+            return Promise.all(initialisedPromises).then(() => {
+                return instance;
+            });
+
+        });
+
+        resolve(self.cache[id]);
+
+    }).then(null, (error) => {
+        throw new ServiceError(id, error);
+    });
+
+    _.each(mappedExtraHandlers, (handler, extraIndex) => {
+        if (handler.onGetComplete) {
+            handler.onGetComplete(definition.extras[extraIndex], extensionApi);
+        }
+    });
+
+    return output;
+}
+
+/**
+ * @return {Promise<Array<String>>} - A list of lint errors
+ */
+function lint () {
+
+    return Promise.all(_.map(this.config.services, (serviceDefinition, serviceId) => {
+
+        const errors = [];
+        const extensionApi = new ExtensionApi(this, serviceId, serviceDefinition);
+
+        const moduleLoader = _.find(this.moduleLoaders, (moduleLoader) => {
+            return moduleLoader.canLoadModule(extensionApi);
+        });
+
+        if (!moduleLoader) {
+            errors.push(`Missing module loader for ${serviceId}`);
+        } else if (moduleLoader.lintLoader) {
+            errors.push(moduleLoader.lintLoader(extensionApi));
+        }
+
+        const initialiser = _.find(this.initialisers, (initialiser) => {
+            return initialiser.canInitialise(extensionApi);
+        });
+
+        if (!initialiser) {
+            errors.push(`Missing initialiser for ${serviceId}`);
+        }
+
+        _.each(serviceDefinition.args, (argDefinition, i) => {
+
+            try {
+                const argResolver = extensionApi.getArgResolver(argDefinition);
+
+                if (argResolver.lintArg) {
+                    errors.push(argResolver.lintArg(argDefinition, extensionApi));
+                }
+            } catch (e) {
+                errors.push(`Missing argResolver at [${i}] for ${serviceId}`);
             }
 
-            const initialiser = _.find(this.initialisers, (initialiser) => {
-                return initialiser.canInitialise(extensionApi);
+        });
+
+        _.each(serviceDefinition.extras, (extraDefinition, i) => {
+
+            const extraHandler = _.find(this.extraHandlers, (extraHandler) => {
+                return extraHandler.canHandleExtra(extraDefinition, extensionApi);
             });
 
-            if (!initialiser) {
-                errors.push(`Missing initialiser for ${serviceId}`);
+            if (!extraHandler) {
+                errors.push(`Missing extraHandler at [${i}] for ${serviceId}`);
+            } else if (extraHandler.lintExtra) {
+                errors.push(extraHandler.lintExtra(extraDefinition, extensionApi));
             }
 
-            _.each(serviceDefinition.args, (argDefinition, i) => {
+        });
 
-                try {
-                    const argResolver = extensionApi.getArgResolver(argDefinition);
+        return Promise.all(errors);
 
-                    if (argResolver.lintArg) {
-                        errors.push(argResolver.lintArg(argDefinition, extensionApi));
-                    }
-                } catch (e) {
-                    errors.push(`Missing argResolver at [${i}] for ${serviceId}`);
-                }
-
-            });
-
-            _.each(serviceDefinition.extras, (extraDefinition, i) => {
-
-                const extraHandler = _.find(this.extraHandlers, (extraHandler) => {
-                    return extraHandler.canHandleExtra(extraDefinition, extensionApi);
-                });
-
-                if (!extraHandler) {
-                    errors.push(`Missing extraHandler at [${i}] for ${serviceId}`);
-                } else if (extraHandler.lintExtra) {
-                    errors.push(extraHandler.lintExtra(extraDefinition, extensionApi));
-                }
-
-            });
-
-            return Promise.all(errors);
-
-        })).then((results) => _.compact(_.flattenDeep(results)));
-
-    }
+    })).then((results) => _.compact(_.flattenDeep(results)));
 
 }
 
